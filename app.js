fcb3a0a4439716c8216c5b977dd1477dd32c2ea0
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-07-08 18:45:29 +0530

    First time parsing done

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const https = require('https');
 
 
 const app = express();
 
+
+
 app.get("/" , (req,res) =>{
   res.sendFile( __dirname + "/index.html");
+  let data = ' ';
+  https.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello/", (response) => {
+    response.on("data" , (chunk) => {
+      data += chunk;
+    });
+    response.on('end' , () => {
+      let parsed  = JSON.parse(data);
+      console.log(parsed);
+      // res.send(parsed);
+    });
+
+  });
+
+
 });
+
 
 app.listen(3000 ,() =>{
   console.log("Server is up and running on port 3000");
