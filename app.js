c68392aec4438c9b77f030c17151191a3d4431cc
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-07-10 12:00:15 +0530

    Used post to send data and also made another function makeRequst to handle https get commands

--- app.js
+++ app.js
@@ -7,22 +7,48 @@ const app = express();
 
 
 app.get("/" , (req,res) =>{
-  // res.sendFile( __dirname + "/index.html");
-  let data = ' ';
-  https.get("https://api.dictionaryapi.dev/api/v2/entries/en/hello/", (response) => {
-    response.on("data" , (chunk) => {
-      data += chunk;
-    });
-    response.on('end' , () => {
-      let parsed  = JSON.parse(data);
-      console.log(parsed);
-      res.send(parsed);
-    });
-
-  });
+  res.sendFile( __dirname + "/index.html");
 
 
 });
+
+function makeRequest(url){
+
+  // Promise constructor is used to create a promise that wraps the asynchronous operation of making an HTTP request.
+  return new Promise((resolve , reject) => {
+     https.get(url, (response) => {
+       let data = ' ';
+      response.on("data" , (chunk) => {
+        data += chunk;
+      });
+
+      // resolve and reject . resolve and reject are two functions that are provided as arguments to the executor function of a promise
+      // resolve(value) is a function used to fulfill the promise. It accepts an optional value as an argument, which represents the eventual
+      // result of the asynchronous operation. When resolve is called, the promise transitions from the pending state to the fulfilled state.
+      response.on('end' , () => {
+        resolve(data);
+
+      });
+      response.on('error' , (error) =>{
+        reject(error);
+      } );
+    });
+  });
+
+}
+
+app.post("/" , async (req,res) => {
+
+  let parsedData = await makeRequest("https://api.dictionaryapi.dev/api/v2/entries/en/hello/");
+  console.log(JSON.parse(parsedData));
+  console.log("Hello");
+  // res.send("This is from post request");
+  res.send(JSON.parse(parsedData));
+
+  // let word = parsedData[0].word;
+  // console.log(word);
+
+} );
 
 
 app.listen(3000 ,() =>{
